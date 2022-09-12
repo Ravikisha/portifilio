0bb22df644cefa09de3dcab4dd9b983ef1a67db8
ravikisha <ravikishan63392@gmail.com>
2022-09-12 11:10:22 +0530

    Adding Share Meta

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import "../styles/globals.scss";
 import Head from "next/head";
-import Script from "next/script";
 import Header from "../components/Header";
 import Footer from "../components/footer";
 import { useEffect } from 'react';
@@ -8,6 +7,8 @@ import { useRouter } from 'next/router';
 
 import { Progress } from '../components/progress';
 import {useProgressStore} from '../components/useProgessStore.js';
+import MyImage from '../public/pagepreview.png'
+
 
 function MyApp({ Component, pageProps }) {
   const setIsAnimating = useProgressStore((state) => state.setIsAnimating);
@@ -33,7 +34,17 @@ function MyApp({ Component, pageProps }) {
   }, [router, setIsAnimating]);
   return (
     <>
-      
+      <Head>
+      <meta property="og:title" content="Ravi Kishan:An Introduction" />
+<meta property="og:type" content="portifilio" />
+<meta property="og:image" content={MyImage.src} />
+<meta property="og:url" content="/" />
+<meta name="twitter:card" content="Ravi Kishan:An Introduction" />
+
+<meta property="og:description" content="Ravi Kishan is a full stack developer." />
+<meta property="og:site_name" content="Ravi Kishan:An Introduction" />
+<meta name="twitter:image:alt" content="Ravi Kishan Portifilio" />
+      </Head>
       <Header />
       <Progress isAnimating={isAnimating} />
       <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,13 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
 
+
 export default function Document() {
+  
   return (
     <Html>
       <Head>
+
         <link
           rel="stylesheet"
           href="https://unicons.iconscout.com/release/v4.0.0/css/line.css"
