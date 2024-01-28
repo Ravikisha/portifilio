e5a297a66b658f857a8cf21916e0c2c490674c8a
ravikishan.69 <ravikishan63392@gmail.com>
2024-01-28 11:07:07 +0530

    adding certificate sample data

---
+++ components/data_cert.js
@@ -0,0 +1,18 @@
+export const data = [
+    {
+        name: "Microsoft Certified: Azure Fundamentals",
+        description: "This certification is intended for candidates who are just beginning to work with cloud-based solutions and services or are new to Azure.",
+        image: "/images/azure.png",
+        link: "https://www.youracclaim.com/badges/0c0b6b5f-7c1a-4f3f-8c7a-3a8b6c3c7f6a",
+        linkedin: "https://www.linkedin.com/in/ravikisha/",
+        tags: ["cloud"],
+    },
+    {
+        name: "Microsoft Certified: Azure AI Fundamentals",
+        description: "This certification is intended for candidates with both technical and non-technical backgrounds. Data science and software engineering experience are not required; however, some general programming knowledge or experience would be beneficial.",
+        image: "/images/azure.png",
+        link: "https://www.youracclaim.com/badges/2f0e4b8b-3a16-4b7c-8f9e-8b3e7b6d9e4b",
+        linkedin: "https://www.linkedin.com/in/ravikisha/",
+        tags: ["cloud"],
+    }
+]
