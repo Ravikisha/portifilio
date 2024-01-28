a3606fdbb252286cdb449d7c7d4aff65c3be8db2
ravikishan.69 <ravikishan63392@gmail.com>
2024-01-28 10:20:40 +0530

    adding new filter options

--- components/filter.js
+++ components/filter.js
@@ -26,6 +26,12 @@ const Filter = ({popular,setFiltered,activeFilter,setActiveFilter}) => {
             <button onClick={()=>setActiveFilter('app')}className={`hover:bg-teal-100 bg-white text-teal-800 rounded-full py-3 px-8 shadow-md hover:shadow-2xl transition duration-500  ${activeFilter === 'app' ? ' bg-black text-gray-100 hover:text-gray-100 hover:bg-black font-bold' : ''}`}>
             App Developement
             </button>
+            <button onClick={()=>setActiveFilter('devops')}className={`hover:bg-teal-100 bg-white text-teal-800 rounded-full py-3 px-8 shadow-md hover:shadow-2xl transition duration-500  ${activeFilter === 'app' ? ' bg-black text-gray-100 hover:text-gray-100 hover:bg-black font-bold' : ''}`}>
+            DevOps
+            </button>
+            <button onClick={()=>setActiveFilter('aiml')}className={`hover:bg-teal-100 bg-white text-teal-800 rounded-full py-3 px-8 shadow-md hover:shadow-2xl transition duration-500  ${activeFilter === 'app' ? ' bg-black text-gray-100 hover:text-gray-100 hover:bg-black font-bold' : ''}`}>
+            AI/ML/DS
+            </button>
             <button onClick={()=>setActiveFilter('others')}className={`hover:bg-teal-100 bg-white text-teal-800 rounded-full py-3 px-8 shadow-md hover:shadow-2xl transition duration-500  ${activeFilter === 'others' ? ' bg-black text-gray-100 hover:text-gray-100 hover:bg-black font-bold' : ''}`}>
             Others
             </button>
