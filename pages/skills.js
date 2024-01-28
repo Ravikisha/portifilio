412e4b8cab35fc76383ceb2e69b928e103213a34
ravikishan.69 <ravikishan63392@gmail.com>
2024-01-28 11:07:31 +0530

    update skills page

--- pages/skills.js
+++ pages/skills.js
@@ -1,19 +1,22 @@
-import React from 'react'
-import Languages from '../components/languages';
-import PureParallax from './../components/pureParallax';
-import Head from 'next/head';
+import React from "react";
+import Languages from "../components/languages";
+import PureParallax from "./../components/pureParallax";
+import Head from "next/head";
+import Certificate from "../components/certificate";
 
 const Skills = () => {
   return (
     <>
-    <Head>
+      <Head>
         <title>My Skills</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-    <PureParallax />
-    <Languages />
+      <PureParallax />
+      <Languages />
+      <hr className="my-10 border-gray-300 border-2 w-[80%] mx-auto" />
+      <Certificate />
     </>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
