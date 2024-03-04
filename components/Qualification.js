9da4ed2e31b2316f65fcf3af246d7e3cf461dc6f
ravikishan.69 <ravikishan63392@gmail.com>
2024-03-04 16:16:48 +0530

    Adding Work Experince

--- components/Qualification.js
+++ components/Qualification.js
@@ -108,14 +108,14 @@ const Qualification = () => {
             </div> */}
           </div>
           <div className={toggleQualification === 2 ? "qualification__content qualification__content-active" : "qualification__content"}>
-            {/* <div className="qualification__data">
+            <div className="qualification__data">
               <div>
-                <h3 className="qualification__title">Developer</h3>
+                <h3 className="qualification__title">Full Stack Developer Intern</h3>
                 <span className="qualification__subtitle">
-                  Spain - Institute
+                  ChitiInfotech Pvt. Ltd. - Gwalior
                 </span>
                 <div className="qualification__calendar">
-                  <i className="uil uil-calendar-alt"></i> 2021 - Present
+                  <i className="uil uil-calendar-alt"></i> Jan 2023 - Sep 2023
                 </div>
               </div>
               <div>
@@ -124,23 +124,8 @@ const Qualification = () => {
               </div>
             </div>
 
-            <div className="qualification__data">
-              <div></div>
-              <div>
-                <span className="qualification__rounded"></span>
-                <span className="qualification__line"></span>
-              </div>
-              <div>
-                <h3 className="qualification__title">Programmer</h3>
-                <span className="qualification__subtitle">
-                  Spain - Institute
-                </span>
-                <div className="qualification__calendar">
-                  <i className="uil uil-calendar-alt"></i> 2021 - Present
-                </div>
-              </div>
-            </div>
-            <div className="qualification__data">
+            
+            {/* <div className="qualification__data">
               <div>
                 <h3 className="qualification__title">Tester</h3>
                 <span className="qualification__subtitle">
@@ -154,25 +139,8 @@ const Qualification = () => {
                 <span className="qualification__rounded"></span>
                 <span className="qualification__line"></span>
               </div>
-            </div>
-
-            <div className="qualification__data">
-              <div></div>
-              <div>
-                <span className="qualification__rounded"></span>
-                <span className="qualification__line"></span>
-              </div>
-              <div>
-                <h3 className="qualification__title">Programmer</h3>
-                <span className="qualification__subtitle">
-                  Spain - Institute
-                </span>
-                <div className="qualification__calendar">
-                  <i className="uil uil-calendar-alt"></i> 2021 - Present
-                </div>
-              </div>
             </div> */}
-            <h2 className="mt-5 text-center text-4xl font-bold">No Experince <span className="text-sky-500">Now !</span></h2>
+            {/* <h2 className="mt-5 text-center text-4xl font-bold">No Experince <span className="text-sky-500">Now !</span></h2> */}
           </div>
         </div>
       </section>
