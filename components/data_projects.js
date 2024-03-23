95dbd0614a1306692d53fab5a7d509f9858f925b
ravikishan.69 <ravikishan63392@gmail.com>
2024-03-23 14:27:02 +0530

    Working on Projects

---
+++ components/data_projects.js
@@ -0,0 +1,15 @@
+// Only add Image and Company Image with the PNG Extension
+
+export const data = [
+  {
+    name: "Microsoft Certified: Azure Fundamentals",
+    organization: "Microsoft",
+    description:
+      "This certification is intended for candidates who are just beginning to work with others-based solutions and services or are new to Azure.",
+    image: "mlsa.jpg",
+    link: "https://www.youracclaim.com/badges/0c0b6b5f-7c1a-4f3f-8c7a-3a8b6c3c7f6a",
+    linkedin: "https://www.linkedin.com/in/ravikisha/",
+    tags: ["academic"],
+    skills: ["JavaScript", "Python"],
+  },
+];
