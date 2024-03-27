b734d94ff19837a3766a77221ba99611f0c2db70
ravikishan.69 <ravikishan63392@gmail.com>
2024-03-27 21:55:47 +0530

    Adding Udemy and Data Certification

--- components/data_cert.js
+++ components/data_cert.js
@@ -11,6 +11,16 @@ export const data = [
     linkedin: "https://www.linkedin.com/posts/ravi-kishan-62ab51221_microsoftstudentambassador-betacomplete-empoweringcommunities-activity-7161911263361392640-172W?utm_source=share&utm_medium=member_desktop",
     tags: ["academic"],
     skills: ["JavaScript", "Python"],
-  },
+  },{
+    name: "The Web Developer Bootcamp 2024",
+    organization: "udemy",
+    description:
+      "This certification is awarded to students who have completed the Web Developer Bootcamp 2024 course on Udemy. The course covers HTML, CSS, JavaScript, Node.js, and more.",
+    image: "WebDevelopment.jpg",
+    link: "https://www.udemy.com/certificate/UC-adf38f62-4798-436a-95b3-b5eb0b474eaf/",
+    linkedin: "https://www.linkedin.com/posts/ravi-kishan-62ab51221_webdevelopment-udemycertificate-coltsteele-activity-7171897874681729024--T8O?utm_source=share&utm_medium=member_desktop",
+    tags: ["academic"],
+    skills: ["JavaScript", "HTML", "CSS", "Node Js", "Express Js", "React"]
+  }
   
 ];
